9a442187a3ec95d754a637e0c893d6f7fe3b8bcb
RyzenXRS <122577332+RyzenXRS@users.noreply.github.com>
2025-06-26 08:51:19 +0700

    - Fix

--- quick_sort.py
+++ quick_sort.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def quick_sort(data, key, descending=False):
     if len(data) <= 1:
         return data
@@ -20,26 +19,4 @@ def quick_sort(data, key, descending=False):
         greater = [x for x in data[1:] if convert(x) > pivot_val]
 
     return quick_sort(less, key, descending) + [pivot] + quick_sort(greater, key, descending)
-=======
-def quick_sort(data, key, descending=False):
-    if len(data) <= 1:
-        return data
 
-    pivot = data[0]
-
-    try:
-        pivot_val = float(pivot[key])
-        convert = lambda x: float(x[key])
-    except:
-        pivot_val = pivot[key].lower()
-        convert = lambda x: x[key].lower()
-
-    if descending:
-        less = [x for x in data[1:] if convert(x) > pivot_val]
-        greater = [x for x in data[1:] if convert(x) <= pivot_val]
-    else:
-        less = [x for x in data[1:] if convert(x) <= pivot_val]
-        greater = [x for x in data[1:] if convert(x) > pivot_val]
-
-    return quick_sort(less, key, descending) + [pivot] + quick_sort(greater, key, descending)
->>>>>>> 997a80b47e2bb495451adde628747f8587d1a13f
